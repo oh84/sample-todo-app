5d9e63a08768d18630e90e628274df372ad7093d
oh84 <100363393+oh84@users.noreply.github.com>
2025-01-12 12:01:23 +0900

    タスクを更新するAPIを追加

--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -4,7 +4,9 @@ module Types
   class MutationType < Types::BaseObject
     
     # タスク作成
-
     field :create_task, mutation: Mutations::CreateTask, description: "Create a new task"
+
+    # タスク更新
+    field :update_task, mutation: Mutations::UpdateTask, description: "Update a task"
   end
 end
